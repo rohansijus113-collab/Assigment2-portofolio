53ee03d267e4de82e23c989b73fde024b22720ed
rohansijus113-collab <rohan.siju.s.113@kalvium.community>
2025-09-24 15:26:55 +0530

    Add files via upload

---
+++ java.js
@@ -0,0 +1,13 @@
+ const heroText = document.getElementById("hero-text");
+ if (heroText) {
+   const text = "Welcome to My Portfolio 🚀";
+   let i = 0;
+
+   function typeWriter() {
+     if (i < text.length) {
+       heroText.textContent += text.charAt(i);
+       i++;
+       setTimeout(typeWriter, 100);
+     }
+   }
+  typeWriter();   }
